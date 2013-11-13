4de2b5e19912703d5e88ed7cb548329e261dde76
Tobias Perelstein <Tobias@Tobias-Perelsteins-MacBook-Pro.local>
2013-11-13 01:00:42 -0500

    First python program.........EVER

---
+++ hackathonlist.py
@@ -0,0 +1,10 @@
+import requests
+import simplejson
+
+hackathons = requests.get("http://hackerleague.org/api/v1/hackathons.json")
+hackathons = simplejson.loads(hackathons.text)
+
+for hackathon in hackathons:
+    print hackathon["name"]
+
+
