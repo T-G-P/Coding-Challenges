9a6e5b5474829f1a415ef92d21089117cba9924f
Tobias Perelstein <tperel88@vi.cs.rutgers.edu>
2013-11-15 03:03:52 -0500

    Attempt at printing out a list of hackathons part 1

--- app.py
+++ app.py
@@ -1,11 +1,17 @@
 from flask import Flask
-
+from hackathonlist import get_hackathons
 #Instantiate flask object
 app = Flask(__name__)
 
 #Apply decorator to function with '@' where app.py resides
 @app.route('/', methods=["get", "post"])
+
+
 def index():
-    return "Hello World"
+    hackathon_list = ""
+
+    for hackathon in get_hackathons():
+        hackathon_list+=hackathon+'\n'
+    return hackathon_list
 
 app.run(host="0.0.0.0", port=5000)
