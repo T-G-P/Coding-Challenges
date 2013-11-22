ea3a15102bd1344c63de48cfd93bdbad3da4ed60
Tobias Perelstein <tobias.perelstein@gmail.com>
2013-11-21 22:33:11 -0500

    Got linkable text rather than a list of entries

--- app.py
+++ app.py
@@ -1,17 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
 from hackathonlist import get_hackathons
+
 #Instantiate flask object
 app = Flask(__name__)
 
 #Apply decorator to function with '@' where app.py resides
 @app.route('/', methods=["get", "post"])
-
-
 def index():
-    hackathon_list = ""
-
-    for hackathon in get_hackathons():
-        hackathon_list+=hackathon+'\n'+'<br>'
-    return hackathon_list
+    hackathon_list = get_hackathons()
+    #hackathon_list = hackathon_list is a keyword argument.
+    return render_template("index.html", hackathon_list=hackathon_list)
 
 app.run(host="0.0.0.0", port=5000)
