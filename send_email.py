fc9c1cf8673d0dd28ce70c770e4b291efd702d7f
tobias <tobias.perelstein@gmail.com>
2014-03-22 13:22:25 -0400

    ..

--- send_email.py
+++ send_email.py
@@ -1,8 +1,9 @@
 from constants import username, password
 from validate_email import validate_email
 import sendgrid
+import DNS
 
-def sendgrid_email(user_email,from_email,subject,msg):
+def sendgrid_email(user_email):
     #Securely connect to SendGrid
     sg = sendgrid.SendGridClient(username, password, secure=True)
     #Make Empty Message
@@ -12,19 +13,19 @@ def sendgrid_email(user_email,from_email,subject,msg):
     #set the subject of the email
     message.set_subject(subject)
     #set the body of the message
-    message.set_text(msg)
+    message.set_text(email_text)
     #set the message from field
     message.set_from(from_email)
     #send the message
     #print "Sending the message to: "+user_email
     status, msg = sg.send(message)
 
-#This function takes comma delimited string and parses the file for all valid emails. 
-def send_emails(to_emails,from_email, subject, msg):
+
+def send_emails(to_emails,from_email,subject,msg):
     string_array = to_emails.split(',')
     for user_email in string_array:
         user_email = user_email.strip(' ')
         is_valid = validate_email(user_email)
         if is_valid:
-            sendgrid_email(user_email, from_email, subject, msg)
+            sendgrid_email(user_email,from_email,subject,msg)
 
