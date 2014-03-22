50d969f4f256180538b0f0beef7cc3b8e8cb334d
tobias <tobias.perelstein@gmail.com>
2014-03-22 03:54:42 -0400

    Flask Appage on its way

---
+++ app.py
@@ -0,0 +1,25 @@
+from flask import Flask, render_template, request
+from constants import username, password
+import requests
+import urllib2
+import os
+import with_sendgrid
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET'])
+def index():
+    return render_template('index.html')
+
+@app.route('/submit', methods=['POST'])
+def submit():
+    from_email = request.form['from_email']
+    to_email = request.form['to_email']
+    subject = request.form['subject']
+    msg = request.form['msg']
+    return render_template('results.html')
+
+
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT",5000))
+    app.run(host='0.0.0.0', port=port)
