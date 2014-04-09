f7cfce76caaf02e743059c72f8e7e198fee61e7a
Tobias Perelstein <tobias.perelstein@gmail.com>
2014-04-09 01:23:26 -0400

    Building the hashtable of hashtables..

--- challenge2/challenge.java
+++ challenge2/challenge.java
@@ -15,32 +15,36 @@ public class challenge{
                 Arrays.sort(result);
                 for (int i = 0; i<result.length; i++){
                     for(int j = 1; j<result.length; i++){
-                        if(!bandsPairs.containsKey(result[i]){
-                            HashMap<String,Integer> pairs = new HashMap(String,Integer>();
-                            pairs.put(result[j],1));
+                        //doesn't contain the key, add key, and then add all pairs associated with this key
+                        if(bandPairs.containsKey(result[i])){
+                            HashMap<String,Integer> pairs = new HashMap<String,Integer>();
+                            pairs.put(result[j],1);
                             bandPairs.put(result[i],pairs);
                         }
                         else{
-                            bandPairs.get(result[i]).put(result[j],pairs.get(result[j]) + 1));
+                            //contains the key, so pairs hash table already exists and check if pair exists
+                            if(bandPairs.get(result[i]).containsKey(result[j])){
+                                bandPairs.get(result[i]).put(result[j], pairs.get(result[j]) + 1);
+                            }
                         }
                     }
                 }
             }
             br.close();
-            }catch(FileNotFoundException e){
-                System.out.println("please make sure the file you entered exists");
-                System.out.println("USAGE: java challenge (filename)");
-            }catch(IOException e){
-                System.out.println("please make sure the file you entered exists");
-                System.out.println("USAGE: java challenge (filename)");
-            }
+        }catch(FileNotFoundException e){
+            System.out.println("please make sure the file you entered exists");
+            System.out.println("USAGE: java challenge (filename)");
+        }catch(IOException e){
+            System.out.println("please make sure the file you entered exists");
+            System.out.println("USAGE: java challenge (filename)");
+        }
 
     }
 
     /*private static void printResult(HashMap<String,String> band){
-        for (String key : bandPairs.keySet()) {
-            System.out.println("Key = " + key + "\nValue = "+band.get(key));
-        }
+      for (String key : bandPairs.keySet()) {
+      System.out.println("Key = " + key + "\nValue = "+band.get(key));
+      }
 
     }
     */
