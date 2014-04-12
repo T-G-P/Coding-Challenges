10b8e49bf77387ceac30caa0e74fe9d0a3627c9d
Tobias Perelstein <tobias.perelstein@gmail.com>
2014-04-11 22:16:51 -0400

    WIP

--- challenge3/StringAuditQuestion.java
+++ challenge3/StringAuditQuestion.java
@@ -39,49 +39,64 @@ public abstract class StringAuditQuestion
         public Pair[] auditString(String input) {
 
             Pair [] result = new Pair[input.length()];
-            System.out.println(result);
+            char temp;
+            int count = 0;
+
             for(int i = 0; i<input.length(); i++){
-                System.out.println(result[i]._char);
+                temp = input.charAt(i);
                 for(int j = 0; j<input.length(); j++){
-                    System.out.println(result[i]._char);
-                    if(result[j]!=null){
-                        System.out.println(result[i]._char);
-                        if(result[j]._char == input.charAt(i)){   //check if the pair array contains thsi character. If it does, increment count
-                            System.out.println(result[i]._char);
-                            result[j]._occurances++;
+                    if(input.charAt(j) == temp){
+                        count++;
+                        if(count > 1){
+                            result[i]._occurances++;
                         }
-                        else{       //pair doesn't exist yet
-                            System.out.println(result[i]._char);
-                            Pair myPair = new Pair(input.charAt(i));
+                        //the char doesn't exist yet in the array
+                        else if(count <= 1){
+                            Pair myPair = new Pair(temp);
                             myPair._occurances = 1;
-                            result[j] = myPair;
+                            if(containsChar(temp,result)){
+                                result[i] = null;
+                            }
+                            else{
+                                result[i] = myPair;
+                            }
                         }
+
                     }
                 }
+                count = 0;
             }
 
             Arrays.sort(result,new OccurrenceComparator());
             return result;
         }
+        public boolean containsChar(char c, Pair[] pairs){
+            for(int i = 0; i<pairs.length; i++){
+                if(pairs[i] != null && pairs[i]._char == c){
+                    return true;
+                }
+            }
+            return false;
+        }
+        public class OccurrenceComparator implements Comparator<Pair>{
+            @Override
+                public int compare(Pair p1, Pair p2) {
+
+                    if (p1._occurances > p2._occurances) {
+                        return -1;
+                    }
+                    else if (p1._occurances < p2._occurances) {
+                        return 1;
+                    }
+                    else{
+                        return 0;
+                    }
+                }
+        }
 
         public void run(String input){
             auditString(input);
             reportAudit(input);
-        }
-    }
-    public class OccurrenceComparator implements Comparator<Pair>{
-        @Override
-        public int compare(Pair p1, Pair p2) {
-
-            if (p1._occurances < p2._occurances) {
-                return -1;
-            }
-            else if (p1._occurances > p2._occurances) {
-                return 1;
-            }
-            else{
-                return 0;
-            }
         }
     }
 
