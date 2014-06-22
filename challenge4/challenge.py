5d1f02e74a53e171daae6fd34bb20fcdaac7c773
tobias <tobias.perelstein@gmail.com>
2014-06-22 18:13:42 -0400

    Extracted and printed the word

--- challenge4/challenge.py
+++ challenge4/challenge.py
@@ -1,20 +1,37 @@
+'''
+Sort the characters in the following string:
+
+abcdefghijklmnopqrstuvwxyz_
+
+by the number of times the character appears in the following text (descending):
+For this challenge, I am storing the string in a text file rather than a variable.
+See the attached text file for the string used here. 
+
+'''
 letters = {}
+word=[]
+
 def parse_text():
-    with open('fogcreek', 'r') as f:
+    with open('textfile', 'r') as f:
         for line in f:
-            char_list= list(line) 
+            char_list= list(line.strip('\n')) 
             for char in char_list: 
                 if not char in letters:
                     letters[char]=1
                 else:
                     letters[char]+=1
+
 def print_word():
+    print "Here are the sorted letters"
     for char in sorted(letters, key=letters.get, reverse=True):
         if(char != '_'):
             print(char, letters[char])
+            word.append(char)
         else:
-            break
+           result = ''.join(word)
+           continue 
+
+    print "The secret word is: \n"+result
+
 parse_text()
 print_word()
-
-
