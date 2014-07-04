312ef6cb4945d76ede2066661ba7e1849a97901d
tobias <tobias.perelstein@gmail.com>
2014-07-03 22:14:17 -0400

    Back from my guitar break...

--- CrackingTheCode/Chapter1/ChapterOne_Problem5.java
+++ CrackingTheCode/Chapter1/ChapterOne_Problem5.java
@@ -1,7 +1,12 @@
 /*Write a method to replace all spaces in a string with ‘%20’.*/
+
 public class ChapterOne_Problem5{
     
-    public static String replaceAllSpaces1(String str){
+    public static String replaceAllSpaces1(char[] str){
+        return "";
+    }
+
+    public static String replaceAllSpaces2(String str){
         String result = "";
         for(int i=0; i<str.length(); i++){
             if(str.charAt(i) == ' '){
@@ -14,10 +19,6 @@ public class ChapterOne_Problem5{
         return result;
     }
 
-    public static String replaceAllSpaces2(String str){
-        return "";
-    }
-
     public static String replaceAllSpaces3(String str){
         String result = str.replaceAll(" ", "%20");
         return result;
@@ -25,7 +26,8 @@ public class ChapterOne_Problem5{
 
     public static void main(String[] args){
         String test = "hello world dawg";
-        System.out.println((replaceAllSpaces1(test)));
+        System.out.println((replaceAllSpaces1(test.toCharArray()));
+        System.out.println((replaceAllSpaces2(test)));
         System.out.println((replaceAllSpaces3(test)));
     }
 }
