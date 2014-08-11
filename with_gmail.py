ecc7db24306b2978c209f9e598f5a7202585a2d6
tobias <tobias.perelstein@gmail.com>
2014-08-11 03:15:38 -0400

    Unglobalized message object

--- with_gmail.py
+++ with_gmail.py
@@ -11,24 +11,11 @@ import DNS
 import re
 
 #Global varialbes
-global msg,part,smtpObj
+global part,smtpObj
 GMAIL_SMTP = "smtp.gmail.com"
 GMAIL_SMTP_PORT = 587
 TEXT_SUBTYPE = "plain"
  
-     
-#Create the message
-msg = MIMEMultipart()
-msg["Subject"] = subject 
-msg["From"] = from_email 
-
-#Body of message
-part = MIMEText(email_text, TEXT_SUBTYPE)
-msg.attach(part)
-part = MIMEApplication(open("Resume_Tobias_Perelstein.pdf","rb").read())
-part.add_header('Content-Disposition', 'attachment', filename="Resume_Tobias_Perelstein.pdf")
-msg.attach(part)
-     
 smtpObj = SMTP(GMAIL_SMTP, GMAIL_SMTP_PORT)
 #Identify yourself to GMAIL ESMTP server.
 smtpObj.ehlo()
@@ -37,6 +24,24 @@ smtpObj.starttls()
 smtpObj.ehlo()
 #Login to service
 smtpObj.login(user=from_email, password=gmail_password)
+
+part1 = MIMEText(email_text, TEXT_SUBTYPE)
+part2 = MIMEApplication(open("Tobias Perelstein - Resume.pdf","rb").read())
+part2.add_header('Content-Disposition', 'attachment', filename="Tobias Perelstein - Resume.pdf")
+
+def build_message(recipient):
+
+    #Create the message
+    msg = MIMEMultipart()
+    msg["Subject"] = subject 
+    msg["From"] = from_email 
+    msg["To"] =recipient 
+
+    #Body of message
+    msg.attach(part1)
+    msg.attach(part2)
+    return msg
+     
 
 #This function takes a tab or comma delimited file and parses the file for all valid emails. 
 def send_emails():
@@ -50,7 +55,7 @@ def send_emails():
                 if is_valid:
                     try:
                         print "Sending the message to: "+user_email
-                        msg["To"] = user_email 
+                        msg = build_message(user_email)
                         #Send email
                         smtpObj.sendmail(from_email, user_email.split(), msg.as_string())
                     except SMTPException as error:
