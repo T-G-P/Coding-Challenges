b4cb811c4abf8fb0cc07fe6f3af5752cf681ec8c
Tobias Perelstein <tperelstein@lifeworx.com>
2015-08-19 15:34:19 -0400

    working on this

--- nodeschool/learnyounode/jugglingasync.js
+++ nodeschool/learnyounode/jugglingasync.js
@@ -1,30 +1,28 @@
 var http = require('http');
-var urls = process.argv[-1, 3];
-var body = '';
-console.log(urls);
 
-// function getResponses(urls, callback) {
-//     for(var url in urls) {
-//         callback(url)
-//     }
-// }
-// 
-// function getResponse(url) {
-//     http.get(url, function(response) {
-//         //encodes response object to be a string
-//         response.setEncoding("utf8");
-//         //while getting the data, append to the body string
-//         response.on("data", function(data){
-//             body += data;
-//         });
-//         response.on("end", function() {
-//             //when the request is over, log the length and the body
-//             console.log(body);
-//         });
-//         response.on("error", function(err){
-//             console.log(err);
-//         });
-//     });
-// }
-// 
-// getResponses(urls, getResponse)
+function getResponses(callback) {
+    for(var i=2; i<process.argv.length; i++) {
+        callback(process.argv[i], i-2);
+    }
+}
+
+function getResponse(url, index) {
+    var body = '';
+    http.get(url, function(response) {
+        //encodes response object to be a string
+        response.setEncoding("utf8");
+        //while getting the data, append to the body string
+        response.on("data", function(data){
+            body += data;
+        });
+        response.on("end", function() {
+            //when the request is over, log the length and the body
+            console.log(body);
+        });
+        response.on("error", function(err){
+            console.log(err);
+        });
+    });
+}
+
+getResponses(getResponse)
