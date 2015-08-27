ec00149046944a6edb29de8839b01f83d471017b
Tobias Perelstein <tobias.perelstein@gmail.com>
2015-08-27 03:13:36 -0400

    parse file for characters

---
+++ moat/p5/problem5.py
@@ -0,0 +1,31 @@
+import argparse
+
+def main():
+    parser = argparse.ArgumentParser(description='P5')
+    parser.add_argument('filename', nargs='?')
+    args = parser.parse_args()
+
+    if args.filename:
+        result = process_file(args.filename)
+        for k, v in result.items():
+            print('%s: %d' % (k, v))
+
+def process_file(file_name):
+    """
+    Won't be good for large files, need to use a
+    generator
+    """
+    valid_chars = "[ \t{}[]\n]+"
+    result = {c: 0 for c in valid_chars}
+    try:
+        with open(file_name) as f:
+            for line in f:
+                for c in line:
+                    if c in valid_chars:
+                        result[c] += 1
+    except IOError:
+        raise
+    return result
+
+if __name__ == '__main__':
+    main()
