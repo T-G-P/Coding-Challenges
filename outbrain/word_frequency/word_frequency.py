db64b08c5999e888df4d822bdb13dbd1b3b0a3c5
Tobias Perelstein <tperelstein@lifeworx.com>
2015-08-28 07:23:45 -0400

    Remove redundant file opening

--- outbrain/word_frequency/word_frequency.py
+++ outbrain/word_frequency/word_frequency.py
@@ -1,30 +1,34 @@
 from json import dumps
 
+records = open('records.txt')
+queries = open('queries.txt')
+
 
 def output(dictionary):
     print(dumps(dictionary, sort_keys=True))
 
 
 def get_word_frequency():
-    with open('queries.txt') as queries:
-        for query in queries:
-            query_set = set(query.rstrip('\n').split(','))
-            check_query(query_set)
+    # with open('queries.txt') as queries:
+    for query in queries:
+        query_set = set(query.rstrip('\n').split(','))
+        res = {}
+        check_query(query_set, res)
 
 
-def check_query(query_set):
-    res = {}
-    with open('records.txt') as records:
-        for record in records:
-            record_set = set(record.rstrip('\n').split(','))
-            if query_set.issubset(record_set):
-                difference = record_set.difference(query_set)
-                for word in difference:
-                    if word not in res:
-                        res[word] = 1
-                    else:
-                        res[word] += 1
+def check_query(query_set, res):
+    # with open('records.txt') as records:
+    for record in records:
+        record_set = set(record.rstrip('\n').split(','))
+        if query_set.issubset(record_set):
+            difference = record_set.difference(query_set)
+            for word in difference:
+                if word not in res:
+                    res[word] = 1
+                else:
+                    res[word] += 1
 
     output(res)
+    records.seek(0)
 
 get_word_frequency()
