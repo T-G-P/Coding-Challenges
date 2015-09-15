38401fc27773ba3950067eb10a0c53a6bdf9667a
Tobias Perelstein <tperelstein@lifeworx.com>
2015-09-15 14:04:06 -0400

    finish concat

--- nodeschool/streamadventure/concat.js
+++ nodeschool/streamadventure/concat.js
@@ -1,28 +1,6 @@
-// var concat = require('concat-stream');
-// 
-// var concatStream = concat(gotString)
-// 
-// 
-// function gotString(body) {
-//   // imageBuffer is all of `cat.png` as a node.js Buffer
-//     var res = body.toString().split('').reverse().join('');
-//     process.stdout.write(res)
-// }
-// 
-// process.stdin.pipe(concatStream).pipe(process.stdout);
+var concat = require('concat-stream');
 
-var concat = require('concat-stream'),
-        ct = function( data ){
-          var text = data.toString(),
-            result = '';
-
-
-      result = text.split('').reverse().join('');
-      process.stdout.write(result);                      
-      console.log( "Output from inside callback" );      
-    };
-
-
-process.stdin.pipe(concat ( ct ) ).pipe(process.stdout); 
-  console.log( "Output after concat" );                  
+process.stdin.pipe(concat(function(buff) {
+    process.stdout.write(buff.toString().split('').reverse().join(''));
+}));
 
