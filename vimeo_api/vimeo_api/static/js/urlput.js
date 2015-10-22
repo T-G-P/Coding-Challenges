b22d3c094b1daa6f5827e5ae6eb79c422b866a33
Tobias <tobias@perelste.in>
2015-10-22 16:55:43 +0000

    Implement progress bar and necessary javascript for calculation

--- vimeo_api/vimeo_api/static/js/urlput.js
+++ vimeo_api/vimeo_api/static/js/urlput.js
@@ -1,15 +1,4 @@
-
-// $.ajax({
-//     type: 'POST', 
-//     //dataType: 'json', // Set datatype - affects Accept header
-//     url: "http://tobias.perelste.in:8002/vimeo/api/v1.0/file", 
-//     headers: {"X-HTTP-Method-Override": "PUT"}, // X-HTTP-Method-Override set to PUT.
-//     data: '{"name": "Dave"}' // Some data e.g. Valid JSON as a string
-// });
-
-
-$(function()
-{
+$(function() {
     // Variable to store your files
     var files;
 
@@ -18,14 +7,12 @@ $(function()
     $('form').on('submit', uploadFiles);
 
     // Grab the files and set them to our variable
-    function prepareUpload(event)
-    {
+    function prepareUpload(event) {
         files = event.target.files;
     }
 
     // Catch the form submit and upload the files
-    function uploadFiles(event)
-    {
+    function uploadFiles(event) {
         event.stopPropagation(); // Stop stuff happening
         event.preventDefault(); // Totally stop stuff happening
 
@@ -33,13 +20,39 @@ $(function()
 
         // Create a formdata object and add the files
         var data = new FormData();
-        $.each(files, function(key, value)
-        {
+        $.each(files, function(key, value) {
             data.append(key, value);
         });
         
         $.ajax({
-            url: 'http://tobias.perelste.in:8002/vimeo/api/v1.0/file',
+            xhr: function() {
+                var xhr = new window.XMLHttpRequest();
+                //Upload progress
+                xhr.upload.addEventListener("progress", function(evt){
+                    if (evt.lengthComputable) {
+                        var percentComplete = (evt.loaded / evt.total) * 100;
+                      //Do something with upload progress
+                        var progressbar = document.getElementById("bar");
+                        progressbar.style.display = 'block';
+                        var text = progressbar.childNodes[1].childNodes[1];
+                        text.innerHTML = percentComplete + '%';
+                        progressbar.style.width = text.innerHTML;
+                        //text.innerHTML = percentComplete;
+                        //console.log(percentComplete);
+                        //alert(percentComplete);
+                    }
+                }, false);
+                //Download progress
+                //xhr.addEventListener("progress", function(evt){
+                //  if (evt.lengthComputable) {
+                //    var percentComplete = evt.loaded / evt.total;
+                //    //Do something with download progress
+                //    console.log(percentComplete);
+                //  }
+                //}, false);
+                return xhr;
+            },
+            url: 'api/v1.0/file',
             headers: {"X-HTTP-Method-Override": "PUT"}, // X-HTTP-Method-Override set to PUT.
             type: 'PUT',
             method: 'PUT',
@@ -48,10 +61,8 @@ $(function()
             dataType: 'json',
             processData: false, // Don't process the files
             contentType: false, // Set content type to false as jQuery will tell the server its a query string request
-            success: function(data, textStatus, jqXHR)
-            {
-                if(typeof data.error === 'undefined')
-                {
+            success: function(data, textStatus, jqXHR) {
+                if(typeof data.error === 'undefined') {
                     // Success so call function to process the form
                     var a = document.createElement('a');
                     var filelist = document.getElementById("filelist");
@@ -60,24 +71,22 @@ $(function()
                     a.appendChild(linkText);
                     a.title = data.url 
                     a.href = data.url;
-                    //document.body.appendChild(a);
                     entry.appendChild(a)
                     filelist.appendChild(entry)
+                    var progressbar = document.getElementById("bar");
+                    progressbar.style.display = 'none';
     
                 }
-                else
-                {
+                else {
                     // Handle errors here
                     console.log('ERRORS: ' + data.error);
                 }
             },
-            error: function(jqXHR, textStatus, errorThrown)
-            {
+            error: function(jqXHR, textStatus, errorThrown) {
                 // Handle errors here
                 console.log('ERRORS: ' + textStatus);
                 // STOP LOADING SPINNER
             }
         });
     }
-
 });
