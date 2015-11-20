627fcc23cc86b919836aa1cfea31ed5542b642e4
T-G-P <tobias.perelstein@gmail.com>
2015-11-20 06:24:54 -0500

    Design/Implement models for sweep api

--- payperks/api/models.py
+++ payperks/api/models.py
@@ -2,40 +2,26 @@ from django.db import models
 
 from django.contrib.auth.models import User
 
-from random import randint
-
-
-class UserProfile(models.Model):
-
-    USER_TYPES = (
-	('0', 'user'),
-	('1', 'admin'),
-    )
-    user = models.OneToOneField(User)
-    user_type = models.CharField(max_length=1, choices=USER_TYPES, default=0)
-    city = models.CharField(max_length=200)
-    state = models.CharField(max_length=200)
-    street = models.CharField(max_length=200)
+from .utils import gen_random_prize_num, gen_random_prize_amount
 
 
 class Sweep(models.Model):
 
-    __PRIZE_RANGE = (1, 100)
-    __PRIZE_AMOUNT_RANGE = (1000, 1000000)
+    num_prizes = models.IntegerField(default=gen_random_prize_num)
+    prize_amount = models.IntegerField(default=gen_random_prize_amount)
+    created_date = models.DateTimeField(auto_now_add=True)
 
-    num_prizes = models.IntegerField(default=self.__gen_random_prize_num)
-    prize_amount = models.IntegerField(default=self.__gen_random_prize_amount)
-    winners = models.ManyToManyField(User)
-
-    def __gen_random_prize_num():
-
-        return randint(*self.__PRIZE_RANGE)
-
-    def __gen_random_prize_num():
-
-        return randint(*self.__PRIZE_AMOUNT_RANGE)
 
 class Drawing(models.Model):
 
     user = models.ForeignKey(User)
-    sweep = models.ForeignKey(User)
+    sweep = models.ForeignKey(Sweep)
+    points = models.IntegerField()
+    is_open = models.BooleanField(default=True)
+    is_winner = models.BooleanField(default=False)
+    prize_claimed = models.BooleanField(default=False)
+    prize_value = models.IntegerField()
+    created_date = models.DateTimeField(auto_now_add=True)
+
+    # class Meta:
+    #     ordering = ['-points', '-created_date']
