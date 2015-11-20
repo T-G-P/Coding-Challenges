8e81f2c3493ff4c1e2ffd14df0eb4125bf78d63a
T-G-P <tobias.perelstein@gmail.com>
2015-11-20 06:26:20 -0500

    Update project urls to account for api and login modules

--- payperks/payperks/urls.py
+++ payperks/payperks/urls.py
@@ -14,9 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  url(r'^blog/', include(blog_urls))
 """
 from django.conf.urls import include, url
-from django.contrib import admin
+
+from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('',
-    url(r'^api/', include('payperks.api.urls')),
-    url(r'^admin/', include(admin.site.urls))
+    url(r'^/', include('login.urls')),
+    url(r'^api/', include('api.urls')),
 )
