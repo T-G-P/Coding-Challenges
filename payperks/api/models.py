58662d53de004a0b38e4e2b6ab69172e0edc4d8e
T-G-P <tobias.perelstein@gmail.com>
2015-11-20 01:20:47 -0500

    Update database models

--- payperks/api/models.py
+++ payperks/api/models.py
@@ -2,6 +2,8 @@ from django.db import models
 
 from django.contrib.auth.models import User
 
+from random import randint
+
 
 class UserProfile(models.Model):
 
@@ -11,10 +13,29 @@ class UserProfile(models.Model):
     )
     user = models.OneToOneField(User)
     user_type = models.CharField(max_length=1, choices=USER_TYPES, default=0)
-    balance = models.IntegerField()
+    city = models.CharField(max_length=200)
+    state = models.CharField(max_length=200)
+    street = models.CharField(max_length=200)
 
-# class Sweep(models.Model):
-#
-#     num_prizes = models.IntegerField()
-#     prize_amount = models.IntegerField()
-#     winners = models.ManyToManyField(User)
+
+class Sweep(models.Model):
+
+    __PRIZE_RANGE = (1, 100)
+    __PRIZE_AMOUNT_RANGE = (1000, 1000000)
+
+    num_prizes = models.IntegerField(default=self.__gen_random_prize_num)
+    prize_amount = models.IntegerField(default=self.__gen_random_prize_amount)
+    winners = models.ManyToManyField(User)
+
+    def __gen_random_prize_num():
+
+        return randint(*self.__PRIZE_RANGE)
+
+    def __gen_random_prize_num():
+
+        return randint(*self.__PRIZE_AMOUNT_RANGE)
+
+class Drawing(models.Model):
+
+    user = models.ForeignKey(User)
+    sweep = models.ForeignKey(User)
