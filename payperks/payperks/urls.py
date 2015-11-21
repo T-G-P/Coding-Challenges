cf2a81b499d75c9c20bb97ed570e7017f0441e7a
T-G-P <tobias.perelstein@gmail.com>
2015-11-21 01:32:42 -0500

    Update project urls

--- payperks/payperks/urls.py
+++ payperks/payperks/urls.py
@@ -13,11 +13,10 @@ Including another URLconf
     1. Add an import:  from blog import urls as blog_urls
     2. Add a URL to urlpatterns:  url(r'^blog/', include(blog_urls))
 """
-from django.conf.urls import include, url
-
 from django.conf.urls import patterns, include, url
 
-urlpatterns = patterns('',
-    url(r'^/', include('login.urls')),
+urlpatterns = patterns(
+    '',
+    url(r'^', include('login.urls')),
     url(r'^api/', include('api.urls')),
 )
