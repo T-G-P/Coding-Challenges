416159479f26e6398eef11cf9f569512a49ade3c
T-G-P <tobias.perelstein@gmail.com>
2015-11-21 15:51:20 -0500

    Update sweepstakes urls

--- payperks/sweepstakes/urls.py
+++ payperks/sweepstakes/urls.py
@@ -4,10 +4,10 @@ from sweepstakes import views
 
 user_urls = patterns(
     '',
-    url(r'^users/(?P<id>[0-9]+)/points$', views.earn_points),
-    url(r'^users/(?P<id>[0-9]+)/sweeps$', views.run_sweeps),
-    url(r'^users/(?P<id>[0-9]+)/drawings/(?P<id>[0-9]+)$', views.check_prize),
-    url(r'^users/(?P<id>[0-9]+)/drawings/(?P<id>[0-9]+)$', views.claim_prize),
+    url(r'^users/points/$', views.earn_points),
+    url(r'^users/sweeps/$', views.run_sweeps),
+    url(r'^users/prize/$', views.check_prize),
+    url(r'^users/prize/$', views.claim_prize),
 )
 
 urlpatterns = patterns(
