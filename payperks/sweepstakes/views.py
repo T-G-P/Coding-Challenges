35f7ccba00f9d522aa3201bfa7accbb08dd26a3b
T-G-P <tobias.perelstein@gmail.com>
2015-11-21 21:26:19 -0500

    Implement and improve functionality of sweepstakes views

--- payperks/sweepstakes/views.py
+++ payperks/sweepstakes/views.py
@@ -1,44 +1,70 @@
 from django.views.decorators.http import require_http_methods
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render_to_response
+from django.template import RequestContext
+from django.views.decorators.csrf import csrf_exempt
 from sweepstakes.models import Sweep, Drawing
 from .utils import admins_only, randint
 
 
+@csrf_exempt
 @login_required
 @require_http_methods(["POST"])
 def earn_points(request):
     user = request.user
-    drawing = user.drawing_set.latest('id')
-    if drawing and drawing.is_open:
-        response = {'status': 'You already have an open drawing'}
+    try:
+        Drawing.objects.filter(user=user).latest('id')
+    except Drawing.DoesNotExist:
+        try:
+            current_sweep = Sweep.objects.latest('id')
+        except Sweep.DoesNotExist:
+            current_sweep = Sweep(initiating_user=user)
+            current_sweep.save()
+
+        current_drawing = Drawing(user=user,
+                                  sweep=current_sweep,
+                                  points=randint(0, 999999))
+        user.drawing_set.add(current_drawing)
+
+        response = {'status': current_drawing.points}
 
     else:
-        new_drawing = Drawing(points=randint(0, 999999))
-        user.drawing_set.add(new_drawing)
-        response = {'status': new_drawing.points}
+        current_drawing = Drawing.objects.latest('id')
+        response = {
+            'status': (
+                'You already have an open drawing'
+                ' with {} points'.format(current_drawing.points)
+            )
+        }
 
-    return render_to_response('prize.html', response,)
+    context_instance = RequestContext(request)
+
+    return render_to_response("points.html",
+                              response,
+                              context_instance=context_instance)
 
 
+@csrf_exempt
 @login_required
 @admins_only
 @require_http_methods(["POST"])
 def run_sweeps(request):
-    user = request.user
 
-    new_sweep = Sweep()
-    new_sweep.save()
+    try:
+        current_sweep = Sweep.objects.latest('id')
+    except Sweep.DoesNotExist:
+        current_sweep = Sweep()
+        current_sweep.save()
 
-    prize_amount = new_sweep.prize_amount
-    num_prizes = new_sweep.num_prizes
+    prize_amount = current_sweep.prize_amount
+    num_prizes = current_sweep.num_prizes
     drawing_prize = prize_amount/num_prizes
-    new_sweep.user = user
+    # current_sweep.user = user
 
     drawings = Drawing.objects.filter(is_open=True)
 
     if drawings:
-        new_sweep.drawing_set.add(*drawings.all())
+        current_sweep.drawing_set.add(*drawings.all())
         winning_drawings = drawings.\
             order_by('-points').\
             all()[:num_prizes]
@@ -49,58 +75,63 @@ def run_sweeps(request):
                 drawing.prize_value = drawing_prize
             drawing.is_open = False
             drawing.save()
-        response = {'status': 'Sweep Completed'}
+        response = {
+            'status': (
+                'Sweep Completed.'
+                '{} prizes awarded'.format(len(winning_drawings))
+            )
+        }
 
     else:
         response = {'status': 'No drawings entered'}
 
-    return render_to_response('sweeps.html', response,)
+    context_instance = RequestContext(request)
+    return render_to_response("sweeps.html",
+                              response,
+                              context_instance=context_instance)
 
 
+@csrf_exempt
 @login_required
-@require_http_methods(["GET"])
-def check_prize(request):
+@require_http_methods(["POST", "GET"])
+def check_or_claim_prize(request):
     user = request.user
-    drawing = user.drawing_set.latest('id')
+    try:
+        drawing = Drawing.objects.filter(user=user).latest('id')
+    except Drawing.DoesNotExist:
+        response = {'status': 'You have no drawings'}
+        return render_to_response('prize.html', response)
 
-    if drawing.is_winner:
-        response = {'status': 'You Won'}
+    if request.method == 'GET':
+        if drawing.is_winner:
+            response = {'status': 'You Won'}
 
-    elif drawing.is_open:
-        response = {'status': 'Drawing is still open'}
+        elif drawing.is_open:
+            response = {'status': 'Current drawing is still open'}
 
-    else:
+    elif request.method == 'POST':
+        if drawing.is_winner and drawing.prize_claimed:
+            response = {'status': 'Prize already claimed'}
+
+        elif drawing.is_open:
+            response = {'status': 'No prize available for claim'}
+        else:
+            drawing.prize_claimed = True
+            drawing.save()
+            address = '{} {} {}'.format(user.userprofile.street,
+                                        user.userprofile.city,
+                                        user.userprofile.state)
+            response = {
+                'status': 'Payment of {} sent to {}'.format(
+                    drawing.prize_value,
+                    address
+                )
+            }
+
+    if not drawing.is_open and not drawing.is_winner:
         response = {'status': 'You Lost'}
 
-    return render_to_response('prize.html', response,)
-
-
-@login_required
-@require_http_methods(["POST"])
-def claim_prize(request):
-    user = request.user
-    drawing = user.drawing_set.latest('id')
-
-    if drawing.prize_claimed:
-        response = {'status': 'Prize already claimed'}
-
-    elif drawing.is_open:
-        response = {'status': 'Drawing is still open'}
-
-    elif not drawing.is_winner:
-        response = {'status': 'You Lost'}
-
-    else:
-        drawing.prize_claimed = True
-        drawing.save()
-        address = '{} {} {}'.format(user.userprofile.street,
-                                    user.userprofile.city,
-                                    user.userprofile.state)
-        response = {
-            'status': 'Payment of {} sent to {}'.format(
-                drawing.prize_value,
-                address
-            )
-        }
-
-    return render_to_response('prize.html', response,)
+    context_instance = RequestContext(request)
+    return render_to_response("prize.html",
+                              response,
+                              context_instance=context_instance)
