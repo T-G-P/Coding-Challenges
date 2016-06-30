9fb7f29acd011f740708af6a2c619f4f3c75acac
Simon Perelstein <tobias.perelstein@gmail.com>
2016-06-29 22:09:20 -0400

    Add soft deletion and server path migrations on images table

---
+++ database/migrations/2016_06_29_221651_add_server_path_column_to_images.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddServerPathColumnToImages extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('images', function (Blueprint $table) {
+            $table->string('serverpath', 255)->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('images', function (Blueprint $table) {
+            $table->dropColumn('serverpath');
+        });
+    }
+}
