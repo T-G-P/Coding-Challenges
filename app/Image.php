d7d4c1310a82715ec13302a7f9101a000459b759
Simon Perelstein <tobias.perelstein@gmail.com>
2016-06-29 22:09:49 -0400

    Update Image model for soft deletion

--- app/Image.php
+++ app/Image.php
@@ -3,10 +3,16 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Image extends Model
 {
+
+    use SoftDeletes;
+
     protected $fillable = ['filename', 'user_id', 'metadata'];
+
+    protected $dates = ['deleted_at'];
 
     public function comments()
     {
