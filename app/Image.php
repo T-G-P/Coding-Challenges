edfa133b2c0f1b6170f6af6c6ddfae2667fe1896
Simon Perelstein <tobias.perelstein@gmail.com>
2016-06-30 10:36:01 -0400

    Add reverse relationship to image model

--- app/Image.php
+++ app/Image.php
@@ -14,6 +14,11 @@ class Image extends Model
 
     protected $dates = ['deleted_at'];
 
+    public function user()
+    {
+        return $this->belongsTo('App\User');
+    }
+
     public function comments()
     {
         return $this->hasMany('App\Comment');
